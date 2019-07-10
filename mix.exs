e9737cfdf5643093f25f80f125fd9c5303502d5c
Craig S. Cottingham <craig@cottingham.net>
2019-07-10 12:25:53 -0500

    Compile files in spec/support when testing

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Abbrev.MixProject do
       app: :abbrev,
       version: "0.1.0",
       elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [espec: :test],
       aliases: aliases(),
@@ -19,6 +20,10 @@ defmodule Abbrev.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "spec/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
